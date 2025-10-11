bbe6f54397ebeea9b019437718eeea4abc6d4dd2
BaileysCodes <baileyouimet034@gmail.com>
2025-10-11 19:04:03 -0400

    Update but not Done preprocess.py

--- workshop1/preprocess.py
+++ workshop1/preprocess.py
@@ -1,29 +1,38 @@
 # Bailey
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
+import re
 def clean_and_tokenize(text: str) -> list[str]:
-    """
-    Input:  raw document text
-    Output: list of lowercase tokens with punctuation removed
-    """
+    #takes raw text and lowercases it + removes punctuation
+    lowerCaseText = text.lower()
+    text = re.sub(r'[^\w\s]','',text)
+    tokens = text.split()
+    return tokens
 
 def lemmatize_terms(tokens: list[str]) -> list[str]:
-    """
-    Input:  list of word tokens
-    Output: list of base-form words (e.g., "superconductors" -> "superconductor")
-    Use WordNetLemmatizer to lemmatize terms.
-    """
+    # Use WordNetLemmatizer to lemmatize terms.
+    lemmatize = WordNetLemmatizer()
+    lemmatizedTokens = []
+    for token in tokens:
+        base = lemmatize.lemmatize(token)
+        lemmatizedTokens.append(base)
+    return lemmatizedTokens
+    
 
 def remove_common_words(tokens: list[str]) -> list[str]:
-    """
-    Input:  list of tokens
-    Output: list of tokens excluding stop words like "the", "is", etc.
-    Use stopwords to remove common words.
-    """
+    #take tokens and remove common words 
+    stop = set(stopwords.words('english')
+    outputTokens = []
+    for token in tokens:
+        if token not in stop:
+            outputTokens.append(token)
+    return outputTokens
 
 def preprocess_text(text: str) -> list[str]:
-    """
-    Input:  raw document text
-    Output: fully cleaned list of meaningful terms
-    Combines the above three steps in order.
-    """
+    #return cleaned list of terms
+    tokens = cleaned_and_tokenize(text)
+    tokens = lemmatize_terms(tokens)
+    tokens = remove_common_words(tokens)
+
+    return tokens
+    
