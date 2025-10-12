f428d2f2276bb2cbb6f4b77c667bc063ff4b01eb
BaileysCodes <baileyouimet034@gmail.com>
2025-10-12 09:08:02 -0400

    Finished preprocess.py

--- workshop1/preprocess.py
+++ workshop1/preprocess.py
@@ -4,24 +4,24 @@ from nltk.corpus import stopwords
 import re
 def clean_and_tokenize(text: str) -> list[str]:
     #takes raw text and lowercases it + removes punctuation
-    lowerCaseText = text.lower()
-    text = re.sub(r'[^\w\s]','',text)
-    tokens = text.split()
+    cleanText = text.lower()
+    cleanText = re.sub(r'[^\w\s]','',cleanText)
+    tokens = cleanText.split()
     return tokens
 
 def lemmatize_terms(tokens: list[str]) -> list[str]:
-    # Use WordNetLemmatizer to lemmatize terms.
-    lemmatize = WordNetLemmatizer()
+    #use WordNetLemmatizer to lemmatize terms.
+    lemmatizer = WordNetLemmatizer()
     lemmatizedTokens = []
     for token in tokens:
-        base = lemmatize.lemmatize(token)
+        base = lemmatizer.lemmatize(token)
         lemmatizedTokens.append(base)
     return lemmatizedTokens
     
 
 def remove_common_words(tokens: list[str]) -> list[str]:
     #take tokens and remove common words 
-    stop = set(stopwords.words('english')
+    stop = set(stopwords.words('english'))
     outputTokens = []
     for token in tokens:
         if token not in stop:
@@ -30,7 +30,7 @@ def remove_common_words(tokens: list[str]) -> list[str]:
 
 def preprocess_text(text: str) -> list[str]:
     #return cleaned list of terms
-    tokens = cleaned_and_tokenize(text)
+    tokens = clean_and_tokenize(text)
     tokens = lemmatize_terms(tokens)
     tokens = remove_common_words(tokens)
 
