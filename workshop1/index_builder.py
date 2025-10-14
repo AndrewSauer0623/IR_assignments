07b0a5e6f10072b9aea3cc11934f615dc17d1d96
Max Vanderbeek <mk27vand@siena.edu>
2025-10-14 17:29:30 -0400

    Fixed Permutum Index

--- workshop1/index_builder.py
+++ workshop1/index_builder.py
@@ -1,12 +1,20 @@
-# Max
+from typing import Optional
 from tree23 import TreeNode, insert_term
 from preprocess import preprocess_text
-def build_document_index(documents: dict[int, str]) -> TreeNode:
+
+def generate_permuterms(term: str) -> list[str]:
+    term = term + '$'
+    return [term[i:] + term[:i] for i in range(len(term))]
+
+def build_permuterm_index(documents: dict[int, str]) -> Optional[TreeNode]:
+    if not documents:
+        return None
+
     root = None
     for doc_id, raw_text in documents.items():
         terms = preprocess_text(raw_text)
-
         for term in terms:
-            root = insert_term(root, term, doc_id)
+            for permuterm in generate_permuterms(term):
+                root = insert_term(root, permuterm, doc_id)
 
     return root
